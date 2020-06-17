c48f6c72d0b47b9a7b48b2a8ef88face66dfcd5b
Abhinav Anand <manasgmishra@gmail.com>
2020-06-17 22:39:44 +0530

    Create typing_speed_test.py

---
+++ Typing Speed Test/typing_speed_test.py
@@ -0,0 +1,38 @@
+from time import time
+ 
+print()
+print("NO NEW LINE IS THERE, WRITE CONTINUOUSLY(just SPACES)")
+s = "this is a simple paragraph that is meant to be nice and easy" \
+    "to type which is why there will be no commas no periods or any" \
+    "capital letters so i guess this means that it cannot really be" \
+    "considered a paragraph but just a series of run on sentences this" \
+    "should help you get faster at typing as im trying not to use too many" \
+    "difficult words in it although i think that i might start making it hard" \
+    "by including some more difficult letters I'm typing pretty quickly so" \
+    "forgive me for any mistakes."  
+
+words = (len(s.split()))
+ 
+print()
+print(s)
+ 
+print("\nAfter you are done press enter to know your time and speed")
+input("\nPress any key to Start:")
+ 
+try:
+    print("\nTimer Started\n")
+    start = time()
+    t = input()
+    end = time()
+    if t == s:
+        total = round(end - start, 2)
+        print("\nHurray! you typed that correctly")
+        print("Your time was %s seconds" % total)
+        total = int(total) / 60        print("Speed was %s wpm" % (str(words // total)))
+ 
+    else:
+        print("\nWrongly entered")
+        print("Try again")
+ 
+except KeyboardInterrupt:
+    print("")
